2400321b1ba5ebd9d416850549870251fb489bc3
Mariana-Coelho <129106508+Mariana-Coelho@users.noreply.github.com>
2023-04-17 16:06:36 -0300

    Mariana de Lourdes Coelho atividade 2
    Mariana de Lourdes Coelho atividade 2

---
+++ main.dart
@@ -0,0 +1,43 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(ComponenteInicial());
+}
+class ComponenteInicial extends StatelessWidget{
+  void eventoBotao(){
+    print("Clicou");
+  }
+Widget build(BuildContext){
+  return MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Perguntas e respostas"),
+      ),
+      body:Center(
+
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ElevatedButton(onPressed: eventoBotao,
+           child: Text("Enviar")),
+           ElevatedButton(onPressed: eventoBotao,
+           child: Text("Cancelar")),
+            ElevatedButton(onPressed: eventoBotao,
+           child: Text("Salvar")),
+           Column(
+            children: [
+              Text("Aprendendo"),
+              Text("Programação"),
+              Text("Flutter"),
+            ],
+           )
+        ],
+       )
+      )
+    )
+  );
+}
+
+}
