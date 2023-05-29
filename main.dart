1bd3b23c0c15a0119dd3120f13805a49074c8109
Mariana-Coelho <129106508+Mariana-Coelho@users.noreply.github.com>
2023-05-29 19:33:49 -0300

    Atividade_Todas_Tarefas
    Atividade_Mariana_Coelho

--- main.dart
+++ main.dart
@@ -1,74 +1,104 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import './respostas.dart';
 
-main() {
-  runApp(AulaGrupoDois());
+void main() {
+  runApp(QuestionnaireApp());
 }
 
-class AulaGrupoDois extends StatefulWidget {
+class QuestionnaireApp extends StatefulWidget {
   @override
-  State<AulaGrupoDois> createState() => _AulaGrupoDoisState();
+  _QuestionnaireAppState createState() => _QuestionnaireAppState();
 }
 
-class _AulaGrupoDoisState extends State<AulaGrupoDois> {
-  var contador = 0;
-
-  final perguntas = [
-    "Qual a cor favorita?",
-    "Comida favorita?",
-    "Animal que mais gosta?",
-    "O que faz nas horas vagas?"
+class _QuestionnaireAppState extends State<QuestionnaireApp> {
+  final List<Map<String, dynamic>> questions = [
+    {
+      'pergunta': 'Qual é o planeta mais próximo do Sol?',
+      'alternativas': ['Vênus', 'Marte', ' Mercúrio'],
+      'resposta': '',
+    },
+    {
+      'pergunta': 'Quem pintou a Mona Lisa?',
+      'alternativas': ['Vincent van Gogh', 'Pablo Picasso', 'Leonardo da Vinci'],
+      'resposta': '',
+    },
+    {
+      'pergunta': 'Quantas patas tem um cachorro?',
+      'alternativas': ['1', '2', '3', '4'],
+      'resposta': '',
+    },
   ];
 
-  @override
-  void clicou() {
-    setState(() {
-      contador = contador + 1;
-    });
-    print(contador);
-  }
+  int currentQuestionIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Perguntas',
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Perguntas"),
+          title: Text('Perguntas'),
         ),
-        body: Column(
-          children: [
-            Text(perguntas[contador]),
-            respostas(perguntas[contador]),
-            ElevatedButton(onPressed: clicou, child: Text("opçao1")),
-            ElevatedButton(onPressed: clicou, child: Text("opçao2")),
-            ElevatedButton(onPressed: clicou, child: Text("opçao3")),
-            ElevatedButton(onPressed: clicou, child: Text("opçao4")),
-            NovoBotao(
-              texto: "Novo Botão",
-              acao: () {
-                print("Clicou no Novo Botão!");
-              },
-            ),
-          ],
+        body: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                questions[currentQuestionIndex]['pergunta']!,
+                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 16.0),
+              Column(
+                children: List<Widget>.generate(
+                  questions[currentQuestionIndex]['alternativas'].length,
+                  (index) => RadioListTile(
+                    title: Text(questions[currentQuestionIndex]['alternativas'][index]),
+                    value: questions[currentQuestionIndex]['alternativas'][index],
+                    groupValue: questions[currentQuestionIndex]['resposta'],
+                    onChanged: (value) {
+                      setState(() {
+                        questions[currentQuestionIndex]['resposta'] = value;
+                      });
+                    },
+                  ),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    if (currentQuestionIndex < questions.length - 1) {
+                      currentQuestionIndex++;
+                    } else {
+                      // Questionário concluído
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('Questionário concluído'),
+                            content: Text('O questionário foi respondido.'),
+                            actions: [
+                              TextButton(
+                                child: Text('OK'),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    }
+                  });
+                },
+                child: Text(
+                  currentQuestionIndex < questions.length - 1 ? 'Enviar' : 'Finalizar questionário',
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-    );
-  }
-}
-
-class NovoBotao extends StatelessWidget {
-  final String texto;
-  final VoidCallback acao;
-
-  const NovoBotao({Key? key, required this.texto, required this.acao})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: acao,
-      child: Text(texto),
     );
   }
 }
